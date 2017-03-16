36e0253ae71504b92510e6cec3f9b44924e68f02
Moraliev Boyan <Boyan.Moraliev@PCBG.pg.local>
2017-03-16 16:25:00 +0200

    ..

---
+++ CarDealerApp-Skeleton/CarDealerApp/Models/ViewModels/AddUserVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CarDealerApp.Models.ViewModels
+{
+    public class AddUserVM
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
